b00439ee225dd943f5bf0ef8fd4641c5754bfd65
Tee <Pisitchai>
2024-12-17 16:11:28 +0700

    chore:set up microservices

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MicroservicesModule } from './microservices/microservices.module';
 
 @Module({
-  imports: [],
+  imports: [MicroservicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('AUTH_MICROSERVICE') private readonly serviceA: ClientProxy) {}
   getHello(): string {
     return 'Hello World!';
   }
