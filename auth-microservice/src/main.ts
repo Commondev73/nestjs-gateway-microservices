2b93143005c98ab5744dececad3ed8b8547754a1
Tee <Pisitchai>
2025-01-04 05:39:22 +0700

    chore:delete cookie-parser

--- auth-microservice/src/main.ts
+++ auth-microservice/src/main.ts
@@ -1,4 +1,3 @@
-import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
