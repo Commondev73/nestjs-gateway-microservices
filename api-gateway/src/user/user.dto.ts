bf45f6afec0a1d48433afd06fd3226820b2ed270
Tee <Pisitchai>
2025-01-06 22:33:13 +0700

    chore:update user dto

--- api-gateway/src/user/user.dto.ts
+++ api-gateway/src/user/user.dto.ts
@@ -1,5 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 /**
  * Create User Dto
@@ -9,16 +9,21 @@ import { ApiProperty } from '@nestjs/swagger';
  * @typedef {UserCreateDto}
  */
 export class UserCreateDto {
-  @ApiProperty({ example: 'name.lastname', description: 'The name of the user' })
+  @ApiProperty({
+    example: 'name.lastname',
+    description: 'The name of the user',
+  })
   readonly name: string;
 
   @ApiProperty({ example: 'userTest', description: 'The username of the user' })
   readonly username: string;
 
-  @ApiProperty({ example: '123456789', description: 'The password of the user' })
+  @ApiProperty({
+    example: '123456789',
+    description: 'The password of the user',
+  })
   readonly password: string;
 }
-
 
 /**
  * User Update Dto
@@ -28,4 +33,20 @@ export class UserCreateDto {
  * @typedef {UserUpdateDto}
  * @extends {PartialType(UserCreateDto)}
  */
-export class UserUpdateDto extends PartialType(UserCreateDto) {}
+export class UserUpdateDto extends PartialType(UserCreateDto) {
+  @ApiPropertyOptional({
+    example: 'newName.lastname',
+    description: 'The updated name of the user',
+  })
+  readonly name?: string;
+  @ApiPropertyOptional({
+    example: 'newUserTest',
+    description: 'The updated username of the user',
+  })
+  readonly username?: string;
+  @ApiPropertyOptional({
+    example: 'newPassword123',
+    description: 'The updated password of the user',
+  })
+  readonly password?: string;
+}
