a703bf8645f7e5e60723af5f7b1101721619221a
Tee <Pisitchai>
2025-01-06 11:49:19 +0700

    fix:schema

--- user-microservice/src/user/schema/user.schema.ts
+++ user-microservice/src/user/schema/user.schema.ts
@@ -3,6 +3,24 @@ import { Document, Types } from 'mongoose';
 
 @Schema({
   timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: (doc, ret) => {
+      delete ret.__v;
+      ret.id = ret._id;
+      delete ret._id;
+      return ret;
+    },
+  },
+  toObject: {
+    virtuals: true,
+    transform: (doc, ret) => {
+      delete ret.__v;
+      ret.id = ret._id;
+      delete ret._id;
+      return ret;
+    },
+  },
 })
 export class User extends Document {
   @Prop({
