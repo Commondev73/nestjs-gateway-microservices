7efec4797ef2a2b002f411b948de3792cfd8836b
Tee <Pisitchai>
2025-01-07 23:39:21 +0700

    fix:redis config

--- user-microservice/src/redis/redis.module.ts
+++ user-microservice/src/redis/redis.module.ts
@@ -1,22 +1,25 @@
-import { Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-ioredis';
+import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
 import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+@Global()
 @Module({
   imports: [
     CacheModule.registerAsync({
       imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        store: 'redis',
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
         host: configService.get<string>('REDIS_HOST'),
         port: configService.get<number>('REDIS_PORT'),
-        auth_pass: configService.get<string>('REDIS_PASSWORD'),
+        password: configService.get<string>('REDIS_PASSWORD'),
         db: configService.get<number>('REDIS_DB'),
       }),
+      inject: [ConfigService],
     }),
   ],
   providers: [RedisService],
+  exports: [RedisService],
 })
 export class RedisModule {}
